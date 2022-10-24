f83c13002259bda031bf0bc05744abf2feedfb8b
manuelgaleone <galeonemanuel14@gmail.com>
2022-10-24 14:22:29 +0200

    Initial commit snack 1

---
+++ snack_1/main.js
@@ -0,0 +1,60 @@
+/*
+Crea un array di 10 oggetti che rappresentano una zucchina, indicando per ognuna varietà, peso e lunghezza.
+
+Calcola quanto pesano tutte le zucchine.
+*/
+
+//Creo la lista di Zucchine
+let zucchine = [
+    {
+        Varietà: "Zucchina Mario",
+        Peso: Number("10") + " " + "kg",
+        Lunghezza: Number("25") + " " + "cm"
+    },
+    {
+        Varietà: "Zucchina Antonio",
+        Peso: Number("12") + " " + "kg",
+        Lunghezza: Number("20") + " " + "cm"
+    },
+    {
+        Varietà: "Zucchina Mauro",
+        Peso: Number("12") + " " + "kg",
+        Lunghezza: Number("18") + " " + "cm"
+    },
+    {
+        Varietà: "Zucchina Giovanni",
+        Peso: Number("12") + " " + "kg",
+        Lunghezza: Number("29") + " " + "cm"
+    },
+    {
+        Varietà: "Zucchina Alpi",
+        Peso: Number("12") + " " + "kg",
+        Lunghezza: Number("20") + " " + "cm"
+    },
+    {
+        Varietà: "Zucchina Andrea",
+        Peso: Number("12") + " " + "kg",
+        Lunghezza: Number("19") + " " + "cm"
+    },
+    {
+        Varietà: "Zucchina Paolo",
+        Peso: Number("12") + " " + "kg",
+        Lunghezza: Number("25") + " " + "cm"
+    },
+    {
+        Varietà: "Zucchina Manuel",
+        Peso: Number("12") + " " + "kg",
+        Lunghezza: Number("20") + " " + "cm"
+    },
+    {
+        Varietà: "Zucchina Pasquale",
+        Peso: Number("12") + " " + "kg",
+        Lunghezza: Number("5") + " " + "cm"
+    },
+    {
+        Varietà: "Zucchina Lucia",
+        Peso: Number("7") + " " + "kg",
+        Lunghezza: Number("2") + " " + "cm"
+    }
+];
+//console.log(zucchine[0]);
