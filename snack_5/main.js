c61e560baa6b63d95dd672c9397d48d06a5793ab
manuelgaleone <galeonemanuel14@gmail.com>
2022-10-24 17:23:31 +0200

    Fixed snack 5

--- snack_5/main.js
+++ snack_5/main.js
@@ -3,3 +3,25 @@ Scrivi una funzione che accetti tre argomenti: un array e due numeri (“a” pi
 
 La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra “a” e “b”.
 */
+
+//Creo l'array e variabili
+let listaAnimali = ["Leone", "Lupo"];
+
+let numeroA = 1;
+let numeroB = 2;
+
+console.log(pushingElements(listaAnimali, numeroA, numeroB));
+
+//Creo la funzione
+function pushingElements(lista, a, b) {
+
+    if (a < b && b <= listaAnimali.length) {
+
+        lista.push(a)
+        lista.push(b)
+    
+    }
+
+    return listaAnimali
+
+}
