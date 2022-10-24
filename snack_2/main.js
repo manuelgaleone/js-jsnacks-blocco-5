125c4b80563518763539a466f8357f1873264b22
manuelgaleone <galeonemanuel14@gmail.com>
2022-10-24 16:27:56 +0200

    Fixed snack 2

--- snack_2/main.js
+++ snack_2/main.js
@@ -10,53 +10,53 @@ Infine stampa separatamente quanto pesano i due gruppi di zucchine.
 let zucchine = [
     {
         varieta: "Zucchina Mario",
-        kg: 10,
-        lunghezza: Number("25") + " " + "cm"
+        kg: 17,
+        cm: 14
     },
     {
         varieta: "Zucchina Antonio",
-        kg: 12,
-        lunghezza: Number("20") + " " + "cm"
+        kg: 22,
+        cm: 19
     },
     {
         varieta: "Zucchina Mauro",
         kg: 12,
-        lunghezza: Number("18") + " " + "cm"
+        cm: 12
     },
     {
         varieta: "Zucchina Giovanni",
         kg: 2,
-        lunghezza: Number("29") + " " + "cm"
+        cm: 19
     },
     {
         varieta: "Zucchina Alpi",
         kg: 12,
-        lunghezza: Number("20") + " " + "cm"
+        cm: 7
     },
     {
         varieta: "Zucchina Andrea",
         kg: 3,
-        lunghezza: Number("19") + " " + "cm"
+        cm: 8
     },
     { 
         varieta: "Zucchina Paolo",
         kg: 5,
-        lunghezza: Number("25") + " " + "cm"
+        cm: 29
     },
     {
         varieta: "Zucchina Manuel",
-        kg: 8, 
-        lunghezza: Number("20") + " " + "cm"
+        kg: 18, 
+        cm: 21
     },
     {
         varieta: "Zucchina Pasquale",
         kg: 12,
-        lunghezza: Number("5") + " " + "cm"
+        cm: 10
     },
     {
         varieta: "Zucchina Lucia",
         kg: 7,
-        lunghezza: Number("2") + " " + "cm"
+        cm: 13
     }
 ];
 
@@ -65,15 +65,36 @@ let zucchineMeno15 = [];
 let zucchinePiu15 = []; 
 
 //Divido le zucchine e pusho nelle rispettive liste
-zucchine.forEach(element => {
-    
-    if (zucchine.kg < 15) {
-        zucchineMeno15.push(element)
-    } else if (zucchine.kg > 15) {
-        zucchinePiu15.push(element)
-    }
+zucchine.forEach(zucchina => {
 
+    if (zucchina.cm < 15) {
+        zucchineMeno15.push(zucchina)
+    } else if (zucchina.cm > 15) {
+        zucchinePiu15.push(zucchina)
+    }
+    
 });
 
 console.log(zucchineMeno15);
 console.log(zucchinePiu15);
+
+//Calcolo il kg dei due gruppi
+let sommaMeno = 0;
+
+zucchineMeno15.forEach(zucchina => {
+
+    sommaMeno += zucchina.kg;
+
+});
+
+console.log(sommaMeno);
+
+let sommaPiu = 0;
+
+zucchinePiu15.forEach(zucchina => {
+
+    sommaPiu += zucchina.kg;
+
+});
+
+console.log(sommaPiu);
